17b1b1eecac6a671d655a23ceac18790ed683a29
wuyeelok <wuyeelok@gmail.com>
2018-08-11 14:55:30 +0800

    The Happy Path and Beyond

---
+++ tests/com/teamtreehouse/vending/AlphaNumericChooserTest.java
@@ -0,0 +1,27 @@
+package com.teamtreehouse.vending;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AlphaNumericChooserTest {
+
+    AlphaNumericChooser chooser;
+
+    @Before
+    public void setUp() throws Exception {
+        chooser = new AlphaNumericChooser(26, 10);
+    }
+
+    @Test
+    public void validInputReturnsProperLoction() throws Exception{
+        AlphaNumericChooser.Location loc = null;
+
+        loc = chooser.locationFromInput("B4");
+
+
+        assertEquals("Proper row",1, loc.getRow());
+        assertEquals("Proper column",3, loc.getColumn());
+    }
+}

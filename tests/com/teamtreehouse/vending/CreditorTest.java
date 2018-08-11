786c2bcb6981380fe9bf751768d28cbd40079ce7
wuyeelok <wuyeelok@gmail.com>
2018-08-11 10:42:19 +0800

    Add 2 test cases for testing function refund

--- tests/com/teamtreehouse/vending/CreditorTest.java
+++ tests/com/teamtreehouse/vending/CreditorTest.java
@@ -6,12 +6,38 @@ import static org.junit.Assert.*;
 
 public class CreditorTest {
 
-    @Test
-    public void refund() {
-        fail();
-    }
 
     @Test
     public void addingFundsIncrementsAvaliableFunds() {
+        //  Arrange
+        Creditor creditor = new Creditor();
+
+        //  Act
+        creditor.addFunds(25);
+        creditor.addFunds(25);
+
+        //  Assert
+        assertEquals(50, creditor.getAvailableFunds());
+
+    }
+
+    @Test
+    public void refundingReturnsAllAvailableFunds() {
+        Creditor creditor = new Creditor();
+        creditor.addFunds(10);
+
+        int refund = creditor.refund();
+
+        assertEquals(10, refund);
+    }
+
+    @Test
+    public void refundingReturnZeroInAvailableFunds() {
+
+        Creditor creditor = new Creditor();
+        creditor.addFunds(60);
+        creditor.refund();
+
+        assertEquals(0, creditor.getAvailableFunds());
     }
 }
